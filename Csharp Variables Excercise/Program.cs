ec59f7af7eba8ecb040a64b48f1eb488779d98ce
JaredMillsap <jmillsap23@gmail.com>
2021-09-27 15:58:19 -0500

    Csharp Excercise

---
+++ Csharp Variables Excercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Csharp_Variables_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Jared";
+
+            int myAge = 25;
+            char middleInitial = 'W';
+            bool trueorF = true;
+            double myWeight = 204.005;
+            decimal myHeight = 70.1m;
+
+            Console.WriteLine($"Hello, my Name is {myName} {middleInitial}. I am {myAge} years old. I weigh {myWeight} lbs. I am {myHeight} inches tall.");
+          
+        }
+    }
+}
